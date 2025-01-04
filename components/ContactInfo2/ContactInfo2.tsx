d7508cdf43119c920e5de8089803cc3585d19623
Muhammad Faizan Soomro <141871174+Faizan418@users.noreply.github.com>
2025-01-04 12:58:40 +0500

    update now

--- components/ContactInfo2/ContactInfo2.tsx
+++ components/ContactInfo2/ContactInfo2.tsx
@@ -7,7 +7,7 @@ const ContantInfo2 = () => {
             <div className={styles.left_div}>
 
                 <div className={styles.left_div_two}>
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique in excepturi pariatur quia?
+                    <iframe className={styles.frame} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2860.774329162915!2d67.18592927414169!3d24.878773844526794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb339fe82b48417%3A0xd40b075ee3f123c4!2sMalir%2015%20Flyover%2C%20Karachi%2C%20Karachi%20City%2C%20Sindh%2C%20Pakistan!5e1!3m2!1sen!2s!4v1735897977442!5m2!1sen!2s" loading="lazy" ></iframe>
                 </div>
 
             </div>
